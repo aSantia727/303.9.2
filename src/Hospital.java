3ec92ecd9fa3dea483244e9a39dc4fc2747fa433
Aaron Santiago <asantia727@gmail.com>
2024-11-12 15:45:20 -0500

    Add files via upload

---
+++ src/Hospital.java
@@ -0,0 +1,11 @@
+
+public class Hospital {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Surgeon s = new Surgeon();
+		s.Doctor_Details();
+		s.surgeon_Details();
+	}
+
+}
---
+++ src/Surgeon.java
@@ -0,0 +1,7 @@
+
+public class Surgeon extends Doctor{
+	void surgeon_Details() {
+		System.out.println("Surgeon Details...");
+		System.out.println(Department = "Cardio");
+	}
+}
